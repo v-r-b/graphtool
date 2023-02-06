21514af7fbf7083e5eff1f7726683757b9503103
Volker <98320263+v-r-b@users.noreply.github.com>
2023-02-06 15:50:53 +0100

    added javadoc part 1

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"graphtool"},{"l":"graphtool.algorithm"},{"l":"graphtool.graph"},{"l":"graphtool.gui"},{"l":"graphtool.gui.components"},{"l":"graphtool.res"},{"l":"graphtool.utils"}]
